8d198500947945af36c3ce02aa1dde79e1f9619c
Burak Guclu <burak.guclu@tedu.edu.tr>
2026-01-12 00:10:42 +0300

    İlk sürüm: İki nokta seçimi ile çizgi oluşturma ve temel piksel sayımı eklendi

--- otopark_analiz.py
+++ otopark_analiz.py
@@ -4,46 +4,32 @@ import numpy as np
 
 video_path = 'otoparkVideo.mp4'
 pickle_path = 'park_koordinatlari'
-window_name = "Otopark Analizi (Poligon)"
+window_name = "Otopark Analizi"
 
 cap = cv2.VideoCapture(video_path)
-
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(window_name, 1280, 720)
 
-try:
-    with open(pickle_path, 'rb') as f:
-        posList = pickle.load(f)
-except:
-    exit()
+with open(pickle_path, 'rb') as f:
+    posList = pickle.load(f)
 
 def checkParkingSpace(imgPro, imgOriginal):
     spaceCounter = 0
-
-    for points in posList:
-        pts = np.array(points, np.int32)
+    for line in posList:
+        pt1, pt2 = line
+        
         mask = np.zeros(imgPro.shape, np.uint8)
-        cv2.drawContours(mask, [pts], -1, 255, -1)
+        cv2.line(mask, pt1, pt2, 255, 1)
         masked_image = cv2.bitwise_and(imgPro, imgPro, mask=mask)
         count = cv2.countNonZero(masked_image)
-        area = cv2.contourArea(pts)
-        
-        if area < 100: area = 100 
-            
-        density = count / area
 
-        if density < 0.18: 
+        if count < 50:
             color = (0, 255, 0)
-            thickness = 3
             spaceCounter += 1
         else:
             color = (0, 0, 255)
-            thickness = 2
-
-        cv2.polylines(imgOriginal, [pts], True, color, thickness)
-
-    cv2.rectangle(imgOriginal, (20, 20), (270, 70), (0,0,0), -1) 
-    cv2.putText(imgOriginal, f'Bos Alan: {spaceCounter}/{len(posList)}', (30, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+        
+        cv2.line(imgOriginal, pt1, pt2, color, 2)
 
 while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
@@ -57,16 +43,8 @@ while True:
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                          cv2.THRESH_BINARY_INV, 25, 16)
-    imgMedian = cv2.medianBlur(imgThreshold, 5)
-    kernel = np.ones((3, 3), np.uint8)
-    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    checkParkingSpace(imgDilate, img)
-    
+    checkParkingSpace(imgThreshold, img)
     cv2.imshow(window_name, img)
-    
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
-
-cap.release()
-cv2.destroyAllWindows()
--- park_secimi.py
+++ park_secimi.py
@@ -1,17 +1,13 @@
 import cv2
 import pickle
-import numpy as np
 
 video_path = 'otoparkVideo.mp4'
 file_path = 'park_koordinatlari'
-window_name = "Park Yeri Secimi (4 Nokta)"
+window_name = "Park Yeri Secimi"
 
 cap = cv2.VideoCapture(video_path)
 success, img_first_frame = cap.read()
 cap.release()
-
-if not success:
-    exit()
 
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 cv2.resizeWindow(window_name, 1280, 720)
@@ -19,23 +15,20 @@ cv2.resizeWindow(window_name, 1280, 720)
 try:
     with open(file_path, 'rb') as f:
         posList = pickle.load(f)
-        if len(posList) > 0 and not isinstance(posList[0], list): 
-             posList = []
 except:
     posList = []
 
-current_points = []
+temp_point = None
 
 def mouseClick(event, x, y, flags, param):
-    global current_points, posList
+    global temp_point, posList
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        if len(current_points) < 4:
-            current_points.append((x, y))
-        
-        if len(current_points) == 4:
-            posList.append(current_points)
-            current_points = []
+        if temp_point is None:
+            temp_point = (x, y)
+        else:
+            posList.append((temp_point, (x, y)))
+            temp_point = None
             with open(file_path, 'wb') as f:
                 pickle.dump(posList, f)
 
@@ -44,24 +37,17 @@ def mouseClick(event, x, y, flags, param):
             posList.pop()
             with open(file_path, 'wb') as f:
                 pickle.dump(posList, f)
-        current_points = []
 
 cv2.setMouseCallback(window_name, mouseClick)
 
 while True:
     img = img_first_frame.copy()
-
-    for points in posList:
-        pts = np.array(points, np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(img, [pts], True, (255, 0, 255), 2)
-
-    if len(current_points) > 0:
-        pts_curr = np.array(current_points, np.int32)
-        pts_curr = pts_curr.reshape((-1, 1, 2))
-        cv2.polylines(img, [pts_curr], False, (255, 0, 0), 2)
-        for pt in current_points:
-            cv2.circle(img, pt, 4, (0, 0, 255), -1)
+    
+    for line in posList:
+        cv2.line(img, line[0], line[1], (255, 0, 255), 2)
+        
+    if temp_point:
+        cv2.circle(img, temp_point, 5, (0,0,255), -1)
 
     cv2.imshow(window_name, img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
