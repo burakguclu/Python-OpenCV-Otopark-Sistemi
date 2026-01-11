218d2f3dedbac52aa58ad93b44aee67f5a2a706a
Burak Guclu <burak.guclu@tedu.edu.tr>
2026-01-11 23:30:23 +0300

    Görüntü işleme pipeline'ı güncellendi: MedianBlur ve Dilate ile gürültü azaltıldı

--- otopark_analiz.py
+++ otopark_analiz.py
@@ -5,13 +5,16 @@ import numpy as np
 video_path = 'otoparkVideo.mp4'
 pickle_path = 'park_koordinatlari'
 width, height = 107, 48
+window_name = "Otopark Analizi"
 
 cap = cv2.VideoCapture(video_path)
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window_name, 1280, 720)
 
 with open(pickle_path, 'rb') as f:
     posList = pickle.load(f)
 
-def checkParkingSpace(imgPro):
+def checkParkingSpace(imgPro, imgOriginal):
     spaceCounter = 0
     for pos in posList:
         x, y = pos
@@ -24,7 +27,9 @@ def checkParkingSpace(imgPro):
         else:
             color = (0, 0, 255)
 
-        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), color, 2)
+        cv2.rectangle(imgOriginal, pos, (pos[0] + width, pos[1] + height), color, 2)
+    
+    cv2.putText(imgOriginal, f'Bos: {spaceCounter}/{len(posList)}', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 200, 0), 2)
 
 while True:
     if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
@@ -38,8 +43,11 @@ while True:
     imgBlur = cv2.GaussianBlur(imgGray, (3, 3), 1)
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                          cv2.THRESH_BINARY_INV, 25, 16)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+    kernel = np.ones((3, 3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
 
-    checkParkingSpace(imgThreshold)
-    cv2.imshow("Otopark Analizi", img)
+    checkParkingSpace(imgDilate, img)
+    cv2.imshow(window_name, img)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
--- park_secimi.py
+++ park_secimi.py
@@ -4,6 +4,14 @@ import pickle
 width, height = 107, 48
 video_path = 'otoparkVideo.mp4'
 file_path = 'park_koordinatlari'
+window_name = "Park Yeri Secimi"
+
+cap = cv2.VideoCapture(video_path)
+success, img_first_frame = cap.read()
+cap.release()
+
+cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(window_name, 1280, 720)
 
 try:
     with open(file_path, 'rb') as f:
@@ -19,22 +27,16 @@ def mouseClick(event, x, y, flags, param):
             x1, y1 = pos
             if x1 < x < x1 + width and y1 < y < y1 + height:
                 posList.pop(i)
-
     with open(file_path, 'wb') as f:
         pickle.dump(posList, f)
 
+cv2.setMouseCallback(window_name, mouseClick)
+
 while True:
-    img = cv2.imread('first_frame.png') 
-    # Not: İlk frame'i manuel kaydettiğini varsayıyoruz bu aşamada veya videodan çekilir
-    # Kodun çalışması için burayı video okuma ile değiştiriyorum:
-    cap = cv2.VideoCapture(video_path)
-    success, img = cap.read()
-    cap.release()
-    
+    img = img_first_frame.copy()
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
 
-    cv2.imshow("Park Yeri Secimi", img)
-    cv2.setMouseCallback("Park Yeri Secimi", mouseClick)
+    cv2.imshow(window_name, img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
